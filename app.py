5ffba865dfe9728a588f7e144660ee77a252d95c
Khadija Ladan <khadijaladan67@gmail.com>
2020-04-07 17:41:46 +0100

    removed comments

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 import requests
 import bs4
 import json
-# from flask import Flask
-
-
-# app = Flask(__name__)
-# @app.route("/")
-
 
 def twitterScrapper(hashtag):
 
     url = 'https://mobile.twitter.com/search?q='
 
-    # remove the '#' from the query
     if '#' in hashtag :
         hashtag = hashtag.replace('#', '')
 
-    
-    #transferr the source from selenium to beautiful soup
     hashtag_search = requests.get(url + hashtag)
     hashtag_soup = bs4.BeautifulSoup(hashtag_search.text, features="lxml")
 
@@ -25,10 +16,8 @@ def twitterScrapper(hashtag):
     tweetLinks = hashtag_soup.find_all("td", class_="timestamp")
     tweetTexts = hashtag_soup.select('div.dir-ltr')
 
-    #create a list where the dictionary for each tweet is to be stored
     data = []
 
-    #loop through the length of the list of names
     for i in range(len(usernames)):
         dictionary = {}
 
@@ -36,15 +25,10 @@ def twitterScrapper(hashtag):
         dictionary['link_to_tweet'] = url + tweetLinks[i].a["href"]
         dictionary['tweet-text'] = tweetTexts[i].getText().replace('\n', '')
 
-        #add dictionay to data list
         data.append(dictionary)
 
-    #convert the list to json
     data_json = json.dumps(data)
 
     return (data_json)
 
 twitterScrapper('#covid19')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
