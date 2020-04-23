39658617fb1e43165aafdccc24b8cef2214318bd
Khadija Ladan <khadijaladan67@gmail.com>
2020-04-23 12:37:52 +0100

    merged conflicts

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 
 from pandelytics import search
 
-def main():
+if __name__ == "__main__":
     news = search.search("jhu", "Data")
     if news:
         print(json.dumps(news, indent=4))
