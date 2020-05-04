83e59f33c9ed89e69740e2468106ccef6f3027bb
Manasseh Mmadu <mmadumanasseh@gmail.com>
2020-05-04 23:19:15 +0100

    Updates to NCDC News

--- pandelytics/engines/ncdc.py
+++ pandelytics/engines/ncdc.py
@@ -6,47 +6,48 @@ class News(Base):
     url = "https://ncdc.gov.ng/news/press"
 
     def parse_soup(self, soup):
-        links = soup.findAll('a', {'class': 'white-text'})
-        titles = soup.select("article h3")
-        details = soup.find_all('article')
+        data = soup.findAll('tr')
+        results = []
+        for entry in data:
+            result = {}
+            result["title"] = entry.find("h3").text
+            result["link"] = entry.find("a").get("href")
+            result["detail"]  = entry.find("article").text
+            result["img"]  = entry.find("img").get("src")
+            result["date"] = entry.find("h4")
+            results.append(result)
 
-        data = []
-        for i in range(0, len(links)):
-            rdict = {}
-            rdict['title'] = titles[i].get_text()
-            rdict['detail'] = details[i].get_text()
-            rdict['link'] = links[i].get_text()
-            data.append(rdict)
-        return data
+        return results
 
-class Data(Base):
-    name = "NCDC"
-    url = 'http://covid19.ncdc.gov.ng/'
 
-    def parse_soup(self, soup):
-        naija_summary = soup.select('table#custom1')
-        state_summary = soup.select('table#custom3')
+# class Data(Base):
+#     name = "NCDC"
+#     url = 'http://covid19.ncdc.gov.ng/'
 
-        lines = naija_summary[0].find_all('tr')
-        gen_dict = {}
-        for line in lines:
-            tds = line.find_all('td')
-            label = tds[0].getText()
-            gen_dict[label] = tds[1].getText().replace('\n', '')
+#     def parse_soup(self, soup):
+#         naija_summary = soup.select('table#custom1')
+#         state_summary = soup.select('table#custom3')
 
-        headers = state_summary[0].find_all('th')
-        keys = []
-        for header in headers:
-            key = header.getText().replace('\r\n                        ', '')
-            keys.append(key)
+#         lines = naija_summary[0].find_all('tr')
+#         gen_dict = {}
+#         for line in lines:
+#             tds = line.find_all('td')
+#             label = tds[0].getText()
+#             gen_dict[label] = tds[1].getText().replace('\n', '')
 
-        rows = state_summary[0].find_all('tr')
-        state_data = []
-        for i in range(1, len(rows)):
-            tds = rows[i].find_all('td')
-            sdict = {}
-            for j in range(5):
-                sdict[(keys[j])] = tds[j].getText().replace('\n', '')
-            state_data.append(sdict)
+#         headers = state_summary[0].find_all('th')
+#         keys = []
+#         for header in headers:
+#             key = header.getText().replace('\r\n                        ', '')
+#             keys.append(key)
 
-        return(gen_dict, state_data)
+#         rows = state_summary[0].find_all('tr')
+#         state_data = []
+#         for i in range(1, len(rows)):
+#             tds = rows[i].find_all('td')
+#             sdict = {}
+#             for j in range(5):
+#                 sdict[(keys[j])] = tds[j].getText().replace('\n', '')
+#             state_data.append(sdict)
+
+#         return(gen_dict, state_data)
